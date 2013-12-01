6a6d004bef35e2700cb7ab1b917fc6d4fa436e53
Quyse Lert <quyse0@gmail.com>
2013-12-01 17:49:25 +0000

    refactor

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 /* Скрипт, конфигурирующий компиляцию.
  */
 
+var fs = require('fs');
+
 /**
  * добавить завершающий /, если нет
  */
@@ -11,18 +13,61 @@ var ensureDir = exports.ensureDir = function(dir) {
 };
 
 // дополнительные переменные окружения
-var env = exports.env = process.env;
+exports.env = process.env;
 
 // платформа, под которую компилируем
-var platform = exports.platform = process.env.PLATFORM || process.platform;
+exports.platform = process.env.PLATFORM || process.platform;
 // компилятор, которым компилируем (undefined - компилятор по умолчанию)
-var toolchain = exports.toolchain = process.env.TOOLCHAIN;
-// модуль платформы
+exports.toolchain = process.env.TOOLCHAIN;
+
+var addEnvFile = function(envFile) {
+	var data = fs.readFileSync(envFile, 'utf8');
+	var re = /^(\w+)\=(.*)$/gm;
+	var a, env = {};
+	while (a = re.exec(data))
+		env[a[1]] = a[2];
+	addEnv(env);
+};
+
+var addEnv = exports.addEnv = function(env) {
+	for ( var i in env)
+		exports.env[i] = env[i];
+};
+
+// список целей
+var targets = exports.targets = [];
+
+// обработать аргументы
+// аргументы, начинающиеся на дефис - параметры
+var args = process.argv.slice(2);
+for ( var i = 0; i < args.length; ++i) {
+	var arg = args[i];
+	// если параметр
+	if (arg.length > 0 && arg[0] == '-') {
+		switch (arg) {
+		case '-env':
+			if (++i >= args.length)
+				throw new Error('env file not specified for -env');
+			addEnvFile(args[i]);
+			break;
+		case '-platform':
+			if (++i >= args.length)
+				throw new Error('platform not specified for -platform');
+			exports.platform = args[i];
+			break;
+		default:
+			throw new Error('unknown option: ' + arg);
+		}
+	}
+	// иначе цель
+	else
+		targets.push(arg);
+}
+
+var platform = exports.platform;
+
 var platformModule = exports.platformModule = require('./platform-' + platform);
 
-/**
- * получение опций в зависимости от конфигурации
- */
 var getOptions = exports.getOptions = function(options, configuration) {
 	return options.all.concat(options[configuration]);
 };
@@ -33,4 +78,33 @@ exports.executableExt = platformModule.executableExt;
 
 exports.libraryExt = platformModule.libraryExt;
 
-exports.maxRunningProcesses = 2;
+exports.maxRunningProcesses = 4;
+
+// обработать таргеты
+for(var i = 0; i < targets.length; ++i) {
+	// если вида type:file, то добавить расширение, соответствующее типу цели
+	var target = targets[i];
+	var a = /^([^\:]+)\:(.+)$/.exec(target);
+	if (a) {
+		target = a[2];
+		switch (a[1]) {
+		case 'exe':
+		case 'executable':
+			target += exports.executableExt;
+			break;
+		case 'a':
+		case 'lib':
+		case 'library':
+			target += exports.libraryExt;
+			break;
+		case 'o':
+		case 'obj':
+		case 'object':
+			target += exports.objectExt;
+			break;
+		default:
+			throw new Error('invalid target type: ' + a[1]);
+		}
+		targets[i] = target;
+	}
+}
--- index.js
+++ index.js
@@ -5,72 +5,7 @@
 var config = require('./config');
 var ice = require('ice');
 require('./rules');
-var fs = require('fs');
-
-var addEnvFile = function(envFile) {
-	var data = fs.readFileSync(envFile, 'utf8');
-	var re = /^(\w+)\=(.*)$/gm;
-	var a, env = {};
-	while (a = re.exec(data))
-		env[a[1]] = a[2];
-	addEnv(env);
-};
-
-var addEnv = exports.addEnv = function(env) {
-	for ( var i in env)
-		config.env[i] = env[i];
-};
-
-// список целей
-var targets = [];
-
-// обработать аргументы
-// аргументы, начинающиеся на дефис - параметры
-var args = process.argv.slice(2);
-for ( var i = 0; i < args.length; ++i) {
-	var arg = args[i];
-	// если параметр
-	if (arg.length > 0 && arg[0] == '-') {
-		switch (arg) {
-		case '-env':
-			if (++i >= args.length)
-				throw new Error('env file not specified for -env');
-			addEnvFile(args[i]);
-			break;
-		default:
-			throw new Error('unknown option: ' + arg);
-		}
-	}
-	// иначе цель
-	else {
-		// если вида type:file, то добавить расширение, соответствующее типу цели
-		var target = arg;
-		var a = /^([^\:]+)\:(.+)$/.exec(target);
-		if (a) {
-			target = a[2];
-			switch (a[1]) {
-			case 'exe':
-			case 'executable':
-				target += config.executableExt;
-				break;
-			case 'a':
-			case 'lib':
-			case 'library':
-				target += config.libraryExt;
-				break;
-			case 'o':
-			case 'obj':
-			case 'object':
-				target += config.objectExt;
-				break;
-			default:
-				throw new Error('invalid target type: ' + a[1]);
-			}
-		}
-		targets.push(target);
-	}
-}
 
 // запустить компиляцию целей
-for ( var i = 0; i < targets.length; ++i)
-	ice.make(targets[i]);
+for ( var i = 0; i < config.targets.length; ++i)
+	ice.make(config.targets[i]);
